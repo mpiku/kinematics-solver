62d28b4724b343682582c33f00a7c130d0df8dea
MaciejPik <maciej@pikulinski.com>
2017-05-01 18:54:29 +0200

    Add graph.trajectory()

--- src/grapher.m
+++ src/grapher.m
@@ -60,6 +60,13 @@ classdef grapher < handle
             xlabel('t [s]'); ylabel(y_legend);
         end
         function addPlot( element, option, point )
+            % Adds plot to currently active figure.
+            % Input:
+            %  * element - element which the plot is created for
+            %  * option - option on what to plot
+            %  * point - has default value - point of the element for which
+            %   plot is created. If nothing is passed, then plot is created
+            %   for center of the element.
             hold on;
             if nargin < 3
                 grapher.plot( element, option );
@@ -68,6 +75,32 @@ classdef grapher < handle
             end
             grapher.defaultLook();
             hold off;
+        end
+        function trajectory( element, point )
+            % Plot trajectory of the element with line width respective to
+            % current point's velocity in relation to overall point's
+            % velocity.
+            % Input:
+            %  * element - element which the plot is created for
+            %  * point - has default value - point of the element for which
+            %   plot is created. If nothing is passed, then plot is created
+            %   for center of the element.
+            max_line_width = 10;
+            if nargin < 3
+                solution = element.getSolution();
+            else
+                solution = element.getSolutionForPoint( point );
+            end
+            point_x = solution(2, :);
+            point_y = solution(3, :);
+            point_vel = sqrt( solution( 5, :).^2 + solution( 6, : ).^2 );
+            line_width = ceil( max_line_width .* point_vel ./ (max( point_vel ) - min( point_vel ) ));
+            figure; % Create new figure
+            for i=1:( numel(point_x) - 1)
+                line( point_x(i:i+1), point_y(i:i+1), 'LineWidth', line_width(i));
+            end
+            grapher.defaultLook();
+            xlabel('x [m]'); ylabel('y [m]');
         end
     end
     
